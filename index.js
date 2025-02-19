be69a550cc6af7438ac823dbde46f54d7f6d8d35
mylolyd <mylolyd@gmail.com>
2025-02-19 14:39:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const app = express();
 const port = process.env.PORT || 8080;
-const bot = new TelegramBot("7908620487:AAF4g43C8WDQ_MPr2Eo9Dg2XYusyQbvMS6U", { polling: true });
+const bot = new TelegramBot("7908620487:AAF4g43C8WDQ_MPr2Eo9Dg2XYusyQbvMS6U", { polling: false });
 
 app.use(express.json());
 
 // Event saat bot menerima pesan
 bot.on("message", (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, `Halo, ${msg.from.first_name}! Anda mengirim: ${msg.text} dan chatid anda ${msg.from.chat.id}`);
+    bot.sendMessage(chatId, `Halo, ${msg.from.chat.id}! Anda mengirim: ${msg.text} `);
 });
 
 // Endpoint untuk mengirim pesan ke chat tertentu
