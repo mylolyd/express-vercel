a789c04a265ce46ccedc9cc18e0d0f4311d25270
mylolyd <mylolyd@gmail.com>
2025-02-19 13:46:01 +0700

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = '7908620487:AAF4g43C8WDQ_MPr2Eo9Dg2XYusyQbvMS6U';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
